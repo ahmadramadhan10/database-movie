a78dd8d0a81c89ff554353d29d636b6ce1f8f9b5
Ahmad Ramadhan <aaarama21@gmail.com>
2024-03-16 18:47:12 +0700

    edit file database

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,11 +2,3 @@ import {PrismaClient, movies } from "@prisma/client";
 export const data = require("../../movies.json");
 export const prisma:PrismaClient = new PrismaClient();
 export type Film = movies;
-
-
-async function name() {
-    const temp = await prisma.movies.findMany();
-    console.log(temp);
-}
-
-//name();
